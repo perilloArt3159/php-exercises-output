261c6c188db6b22068062b3c3134d5dfa7620855
zen <artperillo.ictdu@spcf.edu.ph>
2022-08-04 01:20:33 +0800

    :recycle: Renamed Files

--- index.php
+++ index.php
@@ -3,11 +3,15 @@
 <html lang="en">
 
 <head>
-
+    <?php include_once "includes/section_head.include.php"; ?>
 </head>
 
 <body>
+    <?php include_once "includes/section_header.include.php"; ?>
+
     <h1>Hello World</h1>
+
+    <?php include_once "includes/section_footer.include.php"; ?>
 </body>
 
 </html>
