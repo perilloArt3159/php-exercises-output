7aa78950c1ba48fda21891ce2e3b4545597c3c70
zen <artperillo.ictdu@spcf.edu.ph>
2022-08-05 16:15:09 +0800

    :sparkles: Task 10 Done (#10)

--- tasks/task_10_code_error.php
+++ tasks/task_10_code_error.php
@@ -32,7 +32,6 @@ $page =
 
                 for ($parent = 0; $parent < $length; $parent++)
                 {
-                    echo print_r ($lists) . '<br>';
                     for ($child = 0; $child < $length - $parent - 1; $child++)
                     {
                         if ($lists[$child] > $lists[$child + 1])
@@ -47,21 +46,84 @@ $page =
                 return $lists;
             }
 
-            $testArray   = [50, 40, 30 ,20 ,20];
-            
-            $sortedArray =  bubbleSort($testArray);
-
-            for($i=0; $i < count($sortedArray); $i++)
+            function fixedBubbleSort($lists)
             {
-                echo $sortedArray[$i] . '<br>';
+                $length = count($lists);
+
+                for ($parent = 0; $parent < $length; $parent++)
+                {
+                    for ($child = 0; $child < $length - $parent - 1; $child++)
+                    {
+                        if ($lists[$child] > $lists[$child + 1])
+                        {
+                            $temp              = $lists[$child];
+                            $lists[$child]     = $lists[$child + 1];
+                            $lists[$child + 1] = $temp;
+                        }
+                    }
+                }
+
+                return $lists;
             }
+
+
+            $testArray = [];
+
+            for ($i = 0; $i < 10; $i++)
+            {
+                array_push($testArray, rand(0, 10000));
+            }
+
+            $errorSortedArray = bubbleSort($testArray);
+            $fixedSortedArray = fixedBubbleSort($testArray);
 
             ?>
 
-            <h1>Explaination</h1>
-            <p>
+            <div class="my-2">
+                <h1 class="font-bold mb-2">
+                    Original Array :
+                </h1>
+                <?php for ($i = 0; $i < count($testArray); $i++) : ?>
+                    <span class="p-2 bg-gray-900 text-white mr-1 rounded shadow">
+                        <?= $testArray[$i] ?>
+                    </span>
+                <?php endfor ?>
+            </div>
 
-            </p>
+            <div class="my-2">
+                <h1 class="font-bold mb-2">
+                    Sorted Array Using Old Bubble Sort :
+                </h1>
+                <?php for ($i = 0; $i < count($testArray); $i++) : ?>
+                    <span class="p-2 bg-gray-900 text-white mr-1 rounded shadow">
+                        <?= $errorSortedArray[$i] ?>
+                    </span>
+                <?php endfor ?>
+            </div>
+
+            <div class="my-2">
+                <h1 class="font-bold mb-2">
+                    Sorted Array Using Fixed Bubble Sort :
+                </h1>
+                <?php for ($i = 0; $i < count($testArray); $i++) : ?>
+                    <span class="p-2 bg-gray-900 text-white mr-1 rounded shadow">
+                        <?= $fixedSortedArray[$i] ?>
+                    </span>
+                <?php endfor ?>
+            </div>
+
+            <div class="my-2">
+                <h1 class="font-bold mb-2">Explaination</h1>
+                <ul class="list-disc">
+                    <li>
+                        $temp = $lists[$child + 1]; should be $temp = $lists[$child];
+                    </li>
+                    <li>
+                        function should return the lists
+                    </li>
+                </ul>
+            </div>
+
         </div>
     </div>
 
